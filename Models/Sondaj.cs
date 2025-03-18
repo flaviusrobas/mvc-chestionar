d91d8dc1801166b2f39ace6dfa5b5467b6552f0e
unknown <flaviusrobas@yahoo.ro>
2025-03-18 21:21:14 +0200

    initializare depozit chestionar

---
+++ Models/Sondaj.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAppChestionar.Models
+{
+    public class Sondaj
+    {
+        public int Id { get; set; }
+        public string Intrebari { get; set; }
+        public string Raspunsuri { get; set; }
+
+        public Sondaj()
+        {
+
+        }
+    }
+}
